ca22b4fc20be70aca1d5c3d77a76a368951e21eb
Ana Lopes <anapedra.mil@gmail.com>
2023-05-16 12:44:08 -0300

    Fix: UserRepository and UserService

--- src/main/java/com/devsuperior/movieflix/Services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/Services/UserService.java
@@ -25,113 +25,25 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
-
 @Service
 public class UserService implements UserDetailsService {
-    private static Logger logger=  LoggerFactory.getLogger(UserService.class);
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
     @Autowired
-    BCryptPasswordEncoder passwordEncoder;
+    private UserRepository repository;
     @Autowired
     private AuthService authService;
-
-    private final ReviewRepository reviewRepository;
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-
-
-    public UserService(ReviewRepository reviewRepository, UserRepository userRepository,
-                       RoleRepository roleRepository) {
-        this.reviewRepository = reviewRepository;
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-
-    }
-
-
     @Transactional(readOnly = true)
-    public Page<UserDTO> findAllPaged(Pageable pageable){
-        Page<User> list=userRepository.findAll(pageable);
-        return list.map(x -> new UserDTO(x));
+    public UserDTO getProfile() {
+        return new UserDTO(authService.authenticated());
     }
-    @Transactional(readOnly = true)
-    public UserDTO findById(Long id){
-        Optional<User> obj=userRepository.findById(id);
-        authService.validateSelfOrMember(id);
-        User entity=obj.orElseThrow(
-                ()-> new ResourceNotFoundException("Id "+id+" not found"));
-        return new UserDTO(entity);
-    }
-
-    @Transactional
-
-    public UserDTO insert(UserInsertDTO dto) {
-
-        var user=new User();
-        copyDtoToEntity(dto,user);
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
-        user=userRepository.save(user);
-        return new UserDTO(user);
-
-
-    }
-    @Transactional
-    public UserDTO update(Long id, UserUpdateDTO userDTO){
-        try {
-            var user= userRepository.getOne(id);
-            copyDtoToEntity(userDTO,user);
-            user=userRepository.save(user);
-            return new UserDTO(user);
-        }
-        catch (EntityNotFoundException e){
-            throw new ResourceNotFoundException("Id " + id + " not found :(");
-        }
-
-    }
-    @Transactional
-    public void deleteById(Long id){
-
-        try {
-            userRepository.deleteById(id);
-        }
-        catch (EmptyResultDataAccessException e){
-            throw new ResourceNotFoundException("Id "+id+" not found!");
-        }
-        catch (DataIntegrityViolationException e){
-            throw new DataBaseException("Integrity violation");
-        }
-
-    }
-
-    private void copyDtoToEntity(UserDTO userDTO,User user){
-        user.setEmail(userDTO.getEmail());
-        user.getRoles().clear();
-
-        for (RoleDTO roleDTO : userDTO.getRoles()){
-            Role role=roleRepository.getOne(roleDTO.getId());
-            user.getRoles().add(role);
-        }
-        user.getReviews().clear();
-        for (ReviewDTO reviewDTO : userDTO.getReviews()){
-            Review review =reviewRepository.getOne(reviewDTO.getId());
-            user.getReviews().add(review);
-        }
-    }
-
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
-        if (user == null){
-            logger.error("User not found: "+username);
-            throw new UsernameNotFoundException("Email not found!");
+        User user = repository.findByEmail(username);
+        if (user == null) {
+            logger.error("User not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
         }
-        logger.info("User found!"+username);
+        logger.info("User found: " + username);
         return user;
-
     }
-
-
-
 }
-
-
--- src/main/java/com/devsuperior/movieflix/controllers/UserResource.java
+++ src/main/java/com/devsuperior/movieflix/controllers/UserResource.java
@@ -5,6 +5,7 @@ import com.devsuperior.movieflix.Services.UserService;
 import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.dto.UserInsertDTO;
 import com.devsuperior.movieflix.dto.UserUpdateDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -14,51 +15,17 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-
 @RestController
-@RequestMapping(value = "/users/profile")
+@RequestMapping(value = "/users")
 public class UserResource {
-
-
-    private final UserService service;
-    public UserResource(UserService service) {
-        this.service = service;
-    }
-
-    @PreAuthorize("hasAnyRole('MEMBER')")
-    @GetMapping
-    public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
-        Page<UserDTO> list = service.findAllPaged(pageable);
-        return ResponseEntity.ok().body(list);
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> findById(@PathVariable("id") Long id) {
-        UserDTO dto = service.findById(id);
-        return ResponseEntity.ok().body(dto);
-    }
-
-    @PostMapping
-    public ResponseEntity<UserDTO> insert(@RequestBody @Valid UserInsertDTO dto) {
-        UserDTO newDto = service.insert(dto);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(newDto.getId()).toUri();
-        return ResponseEntity.created(uri).body(newDto);
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody @Valid UserUpdateDTO dto) {
-        UserDTO newDto = service.update(id, dto);
-        return ResponseEntity.ok().body(newDto);
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        service.deleteById(id);
-        return ResponseEntity.noContent().build();
+    @Autowired
+    private UserService service;
+    @GetMapping(value = "/profile")
+    public ResponseEntity<UserDTO> getProfile() {
+        UserDTO dto = service.getProfile();
+        return ResponseEntity.ok(dto);
     }
 }
-
 
 
 
